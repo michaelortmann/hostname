3c090586be4a9fb85aae23e15324afa30285b875
Michael Ortmann <keine email>
2024-03-07 13:31:53 +0100

    400 -> 144 bytes
    Fix count 256 -> 257
    
    Use return value of sysinfo()
    
    Use stack instead of bss
    
    Use elfkickers sstrip
    
    Use reg0 + reg1 addressing
    
    Use newer binutils
    
    Fix and enhance doc
    
    Add cross assemble/link doc
    
    Add truss test to README
    
    Add .gitignore

--- hostname.s
+++ hostname.s
@@ -1,37 +1,36 @@
 ! SPDX-License-Identifier: MIT
-! Copyright (c) 2006 Michael Ortmann
+! Copyright (c) 2006-2024 Michael Ortmann
 
   .global _start
 
   .text
 
+  .equ COUNT,          257  ! https://docs.oracle.com/cd/E88353_01/html/E37841/sysinfo-2.html
+  .equ EXIT_SUCCESS,   0    ! /usr/include/iso/stdlib_iso.h
+  .equ SI_HOSTNAME,    2    ! /usr/include/sys/systeminfo.h
+  .equ STDOUT_FILENO,  1    ! /usr/include/unistd.h
+  .equ SYSCALL,        0x00 ! https://github.com/illumos/illumos-gate/blob/master/usr/src/uts/sun4u/ml/trap_table.S
+
+  .equ SYS_exit,       1    ! /usr/include/sys/syscall.h
+  .equ SYS_write,      4
+  .equ SYS_systeminfo, 139
+
 _start:
-  mov  2,%o0        ! int command : SI_HOSTNAME = 2
-  set  str0,%o1     ! char *buf
-  mov  256,%o2      ! long count
-  mov  139,%g1      ! long sysinfo(int command, char *buf, long count)
-  ta   0
+  mov  SI_HOSTNAME,%o0      ! int command = SI_HOSTNAME = 2
+  sub  %sp, ((COUNT + 7) / 8) * 8, %o1 ! char *buf, stack instead of bss, align 8
+  mov  COUNT,%o2            ! long count
+  mov  SYS_systeminfo,%g1   ! int sysinfo(int command, char *buf, long count)
+  ta   SYSCALL              ! https://docs.oracle.com/cd/E88353_01/html/E37841/sysinfo-2.html
 
-  set  str0,%o1     ! *str
-  mov  0,%o2        ! len = 0
-  mov  %o1,%o3
-next:
-  ldub [%o3],%o0    ! c = str[len]
-  cmp  %o0,0        ! c == \0 ?
-  be   done
-  inc  %o2          ! len++
-  inc  %o3          ! &c++
-  ba   next
-done:
-  mov  '\n',%o0
-  stb  %o0,[%o3]
+  mov  %o0,%o2              ! size_t nbyte
+  dec  %o0                  ! buf + nbyte - 1 = '\n'
+  mov  '\n',%o3
+  stb  %o3,[%o1 + %o0]
 
-  mov  1,%o0        ! stdout
-  mov  4,%g1        ! write(0, str, len);
-  ta   0
+  mov  STDOUT_FILENO,%o0    ! int fildes = STDOUT_FILENO = 1
+  mov  SYS_write,%g1        ! ssize_t write(int fildes, const void *buf, size_t nbyte)
+  ta   SYSCALL              ! https://docs.oracle.com/cd/E88353_01/html/E37841/write-2.html
 
-  mov  0,%o0        ! n = 0
-  mov  1,%g1        ! exit(n)
-  ta   0
-
-  .lcomm str0 256
+  mov  EXIT_SUCCESS,%o0     ! int status = EXIT_SUCCESS = 0
+  mov  SYS_exit,%g1         ! void exit(int status)
+  ta   SYSCALL              ! https://docs.oracle.com/cd/E88353_01/html/E37841/exit-2.html
--- make.sh
+++ make.sh
@@ -1,9 +1,27 @@
 # SPDX-License-Identifier: MIT
-# Copyright (c) 2006 Michael Ortmann
+# Copyright (c) 2006-2024 Michael Ortmann
 
 # use as and ld from gnu binutils
-# tested with binutils-2.17
+# tested with binutils-2.17 under SunOS
 # dont use sun as and ld
 
-as -R hostname.s -o hostname.o
-ld -s hostname.o -o hostname
+# as -R hostname.s -o hostname.o
+# ld -s hostname.o -o hostname
+
+# binutils can also be compiled with sparc elf target to cross assemble
+# and link to sparc elf
+# tested with binutils 2.42 under GNU/Linux x86-64
+
+# wget http://ftp.gnu.org/gnu/binutils/binutils-2.42.tar.xz
+# tar -Jxf binutils-2.42.tar.xz
+# cd binutils-2.42
+# ./configure --prefix=/home/michael/opt/binutils-2.42-sparc-elf --target=sparc-elf
+# make
+# make install
+
+/home/michael/opt/binutils-2.42-sparc-elf/bin/sparc-elf-as -R hostname.s -o hostname.o
+/home/michael/opt/binutils-2.42-sparc-elf/bin/sparc-elf-ld -s hostname.o -o hostname
+
+# https://www.muppetlabs.com/~breadbox/software/elfkickers.html
+
+sstrip hostname
